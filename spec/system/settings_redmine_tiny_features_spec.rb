26be0ba3a5d9fabbc5e1cb331e97c8e242b4057b
mathieu-mbru <m.brulin@adacis.net>
2021-10-21 09:10:02 +0200

    remettre le test

--- spec/system/settings_redmine_tiny_features_spec.rb
+++ spec/system/settings_redmine_tiny_features_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe "settings_redmine_tiny_features", type: :system do
 
   it "Should active the option empty_available_filters" do
 
-    # log_user('admin', 'admin')
-    # visit 'settings/plugin/redmine_tiny_features'
+    log_user('admin', 'admin')
+    visit 'settings/plugin/redmine_tiny_features'
 
-    # find("input[name='settings[empty_available_filters]']").click
-    # find("input[name='commit']").click
-    # expect(Setting["plugin_redmine_tiny_features"]["empty_available_filters"]).to eq "1"
+    find("input[name='settings[empty_available_filters]']").click
+    find("input[name='commit']").click
+    expect(Setting["plugin_redmine_tiny_features"]["empty_available_filters"]).to eq "1"
 
   end
 end
